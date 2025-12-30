6f7d58fe23fa40e47d369b5eb799041af46b9656
Abhipsa Srivastava <151427629+abhipsa14@users.noreply.github.com>
2025-12-30 10:41:30 +0530

    Time: 0 ms (100%), Space: 17.3 MB (90.41%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -5,21 +5,17 @@
 #         self.next = next
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-
-        slist, blist = ListNode(), ListNode()
-        small, big = slist, blist # dummy lists
+        slist,blist=ListNode(),ListNode()
+        small,big=slist,blist
 
         while head:
-            if head.val < x:
-                small.next = head
-                small = small.next
+            if head.val<x:
+                small.next=head
+                small=small.next
             else:
-                big.next = head
-                big = big.next
-
-            head = head.next
-
-        small.next = blist.next
-        big.next = None # prevent linked list circle
-
+                big.next=head
+                big=big.next
+            head=head.next
+        small.next=blist.next
+        big.next=None
         return slist.next
