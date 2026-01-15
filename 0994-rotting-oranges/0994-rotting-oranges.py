09ae50d2d006ba2085de770f6fdaa3c8dae646d2
Abhipsa Srivastava <151427629+abhipsa14@users.noreply.github.com>
2026-01-15 20:43:12 +0530

    Time: 3 ms (80.52%), Space: 19.4 MB (10.89%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -18,7 +18,7 @@ class Solution:
             total_rotten=len(queue)
             for _ in range(total_rotten):
                 i,j=queue.popleft()
-                for dx,dy in [[-1,0],[0,-1],[0,1],[1,0]]:
+                for dx,dy in [(1,0),(-1,0),(0,1),(0,-1)]:
                     new_i,new_j=i+dx,j+dy
                     if new_i<0 or new_i==rows or new_j<0 or new_j==cols:
                         continue
