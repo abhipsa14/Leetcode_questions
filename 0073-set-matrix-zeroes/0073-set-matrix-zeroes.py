a8f7a26f93354c76754e8e6654c59e2b902f90bb
Abhipsa Srivastava <151427629+abhipsa14@users.noreply.github.com>
2026-01-18 18:35:40 +0530

    Time: 3 ms (70.67%), Space: 20.1 MB (14.36%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -5,14 +5,22 @@ class Solution:
         """
         n=len(matrix)
         m=len(matrix[0])
-        rows=set()
-        cols=set()
+        col0=True
         for i in range(n):
-            for j in range(m):
+            if matrix[i][0]==0:
+                col0=False
+            for j in range(1,m):
                 if matrix[i][j]==0:
-                    rows.add(i)
-                    cols.add(j)
-        for i in range(n):
-            for j in range(m):
-                if i in rows or j in cols:
+                    matrix[i][0]=0
+                    matrix[0][j]=0
+        for i in range(1,n):
+            for j in range(1,m):
+                if matrix[i][0]==0 or matrix[0][j]==0:
                     matrix[i][j]=0
+        if matrix[0][0]==0:
+            for j in range(m):
+                matrix[0][j]=0
+
+        if not col0:
+            for i in range(n):
+                matrix[i][0]=0
